ba20dcf17e84f5a622c43e901fda0655f7548d6f
fernndes <fernand3s.dev@gmail.com>
2026-03-03 19:19:13 -0300

    position + wireframe

--- src/script.js
+++ src/script.js
@@ -10,7 +10,7 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
@@ -27,6 +27,7 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.position.y = 1
 scene.add(camera)
 
 /**
